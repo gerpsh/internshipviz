6cab0af2a24e2c81dc3e9a9f7d9cd92d73e98ed3
Jerry Shaeffer <gps5012@gmail.com>
2015-03-30 11:36:36 -0400

    added jquery and d3

--- misc/get_coordinates.py
+++ misc/get_coordinates.py
@@ -10,6 +10,7 @@ with open("internship.csv", "rU") as c:
     for row in r:
         lat = ""
         lng = ""
+        row[1] = row[1].strip()
         location = row[1]
         location = location.replace(" ", "+")
         full_request = base_request + location
@@ -19,14 +20,15 @@ with open("internship.csv", "rU") as c:
             lat = str(data['results'][0]['geometry']['location']['lat'])
             lng = str(data['results'][0]['geometry']['location']['lng'])
             print("completed: " + row[1])
-            time.sleep(10)
+            time.sleep(1)
         except:
             lat = "error"
             lng = "error"
-        
+            print("error on: " + row[1])
+
         row.append(lat)
         row.append(lng)
-        with(open("internship1.csv", "wb")) as o:
+        with(open("internship1.csv", "a")) as o:
             w = csv.writer(o, delimiter='|', quotechar='"')
             w.writerow(row)
-        
+
