c3a03c94c164d42b0af095463fa32c0976144356
Jerry Shaeffer <gps5012@gmail.com>
2015-04-10 11:59:10 -0400

    basic elements

--- js/internship_main.js
+++ js/internship_main.js
@@ -18,7 +18,7 @@ var masterData = $.parseJSON(getMasterData());
 
 
 //sum number of internships for overal view
-function countSum(year) {
+function sumData(year) {
 	
 	var yearly = d3.nest()
 				   .key(function(d) {return d.year;}).sortKeys(d3.ascending)
@@ -27,21 +27,36 @@ function countSum(year) {
 				   	return {
 				   		"location": interns.location,
 				   		"count": interns.length,
-				   		"avgWage": d3.mean(interns, function(g) { 
-				   			if (g["wage"] != 'unpaid') {
-				   				//console.log(g["wage"]);
-				   				return g["wage"];
-				   			} else {
-				   				return null;
-				   			}
-				   		}),
 				   		/*take max of lat and lng; since all values the same for a location, 
 				   		  true values always get returned*/
-				   		"lat": d3.max(interns, function(g) {return g["lat"]}),
-				   		"lng": d3.max(interns, function(g) {return g["lng"]})
+				   		"coordinates": [d3.max(interns, function(g) {return parseFloat(g["lng"])}), 
+				   						d3.max(interns, function(g) {return parseFloat(g["lat"])})]
 				   	}
 				   }).entries(masterData);
-	return yearly;
+	return yearly[year];
 }
-console.log(JSON.stringify(countSum()));
+
+function industryData(location, year) {
+	var siteD = d3.nest()
+					 .key(function(d) { return d.year; }).sortKeys(d3.ascending)
+					 .key(function(d) { return d.location; })
+					 .key(function(d) { return d.industry; })
+					 .rollup(function(interns) {
+					 	return {
+					 		"location": interns.location,
+					 		"count": interns.length
+					 	}
+					 }).entries(masterData);
+    //console.log(siteD[0]["values"][0]);
+	for(var i in siteD[year]["values"]) {
+		if (siteD[year]["values"][i]["key"] == location) {
+			return siteD[year]["values"][i]["values"]
+				   .sort(function(a,b) { return parseFloat(b.values.count) - parseFloat(a.values.count) });
+		}
+	}
+}
+
+
+
+//console.log(JSON.stringify(siteData("Ann Arbor, Michigan", 0)));
 
